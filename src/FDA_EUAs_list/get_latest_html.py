e3dfdb484e5fb08670eb2baa523b3f5e6ef3f4ea
Alexander James Phillips <ajp.global@gmail.com>
2021-02-08 07:45:17 +0000

    Add latest FDA EUA html page

---
+++ src/FDA_EUAs_list/get_latest_html.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+import os
+import sys
+
+import requests
+
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+sys.path.insert(0, dir_path + "/..")
+
+from common import DATA_DIRECTORY_EUAs
+
+
+
+HTML_DIR = DATA_DIRECTORY_EUAs + "html_pages/"
+
+def HTML_file_path (file_name):
+    return HTML_DIR + file_name + ".htm"
+
+
+
+def get_latest_html ():
+    response = requests.get("https://www.fda.gov/emergency-preparedness-and-response/mcm-legal-regulatory-and-policy-framework/emergency-use-authorization")
+
+    response.raise_for_status()
+
+    date_str = datetime.now().strftime("%Y-%m-%d")
+    file_path_date = HTML_file_path(date_str)
+    file_path_latest = HTML_file_path("latest")
+
+    html = response.text
+
+    with open(file_path_date, "w") as f:
+        f.write(html)
+
+    with open(file_path_latest, "w") as f:
+        f.write(html)
+
+
+
+if __name__ == "__main__":
+    get_latest_html()
